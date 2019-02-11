00e5988aa162539f59cfa0b3a34de2ff76320548
Zoilo Ismael Reyes <zreyes@adm.unapec.edu.do>
2019-02-11 13:19:54 -0400

    Added consideration for .yo-rc, added parameter webPart

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,31 +4,32 @@ module.exports = class extends Generator{
     constructor(args, opts){
         super(args, opts);
 
+        this.argument('webPart', {type: String, required: true});
         this.argument('componentName', {type: String, required: true});
     }
 
     writing() {
         this.fs.copyTpl(
             this.templatePath("component.tsx"),
-            this.destinationPath(this.options.componentName + ".tsx"),
+            this.destinationPath(`src/webparts/${this.options.webPart}/components/${this.options.componentName}/${this.options.componentName}.tsx`),
             {componentName: this.options.componentName}
         );
 
         this.fs.copyTpl(
             this.templatePath("component.module.scss"),
-            this.destinationPath(this.options.componentName + ".module.scss"),
+            this.destinationPath(`src/webparts/${this.options.webPart}/components/${this.options.componentName}/${this.options.componentName}.module.scss`),
             {componentName: this.options.componentName}
         )
 
         this.fs.copyTpl(
             this.templatePath("Icomponentprops.ts"),
-            this.destinationPath( "I" + this.options.componentName + "Props.ts"),
+            this.destinationPath(`src/webparts/${this.options.webPart}/components/${this.options.componentName}/I${this.options.componentName}Props.ts`),
             {componentName: this.options.componentName}
         )
 
         this.fs.copyTpl(
             this.templatePath("Icomponentstate.ts"),
-            this.destinationPath("I" + this.options.componentName + "State.ts"),
+            this.destinationPath(`src/webparts/${this.options.webPart}/components/${this.options.componentName}/I${this.options.componentName}State.ts`),
             {componentName: this.options.componentName}
         )
 
